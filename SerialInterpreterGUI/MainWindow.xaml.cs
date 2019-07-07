b5c2c9264ec0622b27cad33e554644acfe7fe523
Amir <amirasyraf@outlook.com>
2019-07-07 11:56:44 +0800

    Added all files

---
+++ SerialInterpreterGUI/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SerialInterpreterGUI
+{
+    public partial class MainWindow : Window
+    {
+        public static SerialPort Port = new SerialPort("COM1", 19200, Parity.None, 8, StopBits.One);
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Start_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Terminal_Text.Inlines.Add(new Run("Started!\n") { Foreground = Brushes.Green });
+            RunStatus.status = true;
+            Port.DataReceived += new SerialDataReceivedEventHandler(Port_DataReceived);
+            Port.Open();
+        }
+
+        private void Stop_Button_Click(object sender, RoutedEventArgs e)
+        {
+            RunStatus.status = false;
+        }
+    }
+    public static class RunStatus
+    {
+        public static Boolean status { get; set; }
+    }
+}
---
+++ SerialInterpreterGUI/Process.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SerialInterpreterGUI
+{
+    public partial class MainWindow : Window
+    {
+        static string strHostCommand;
+        private void Port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (!RunStatus.status)
+            {
+                Port.Close();
+                return;
+            }
+            strHostCommand = Port.ReadExisting();
+            this.Dispatcher.Invoke(() =>
+            {
+                Terminal_Text.Inlines.Add(new Run(strHostCommand + "\n") { Foreground = Brushes.Black });
+                Terminal_Text_Scroll.ScrollToBottom();
+            });
+        }
+    }
+}
