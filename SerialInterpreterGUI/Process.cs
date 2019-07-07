748018e6c2d04cfdd743747fa844b274af06bd3a
Amir Asyraf <amirasyraf@outlook.com>
2019-07-07 15:28:13 +0800

    Bug fixes and process logic changes

--- SerialInterpreterGUI/Process.cs
+++ SerialInterpreterGUI/Process.cs
@@ -2,49 +2,45 @@
 using System.Collections.Generic;
 using System.IO.Ports;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace SerialInterpreterGUI
 {
     public partial class MainWindow : Window
     {
         static string strHostCommand;
+        private void Start_Listen()
+        {
+            Port = new SerialPort(ComPortList.Text, 19200, Parity.None, 8, StopBits.One);
+            this.Dispatcher.Invoke(() =>
+            {
+                Terminal_Text.Inlines.Add(new Run("Listening...\n") { Foreground = Brushes.Green });
+            });
+            RunStatus.status = true;
+            Port.DataReceived += new SerialDataReceivedEventHandler(Port_DataReceived);
+            Port.Open();
+        }
+        private void Stop_Listen()
+        {
+            RunStatus.status = false;
+            Port.Close();
+            this.Dispatcher.Invoke(() =>
+            {
+                Terminal_Text.Inlines.Add(new Run("Stopped!\n") { Foreground = Brushes.Red });
+            });
+        }
         private void Port_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
-            if (!RunStatus.status)
-            {
-                Port.Close();
-                return;
-            }
             strHostCommand = Port.ReadExisting();
             this.Dispatcher.Invoke(() =>
             {
                 Terminal_Text.Inlines.Add(new Run(strHostCommand + "\n") { Foreground = Brushes.Black });
                 Terminal_Text_Scroll.ScrollToBottom();
             });
-        }
-        private void RefreshComPortList()
-        {
-            // Determain if the list of com port names has changed since last checked
-            string selected = RefreshComPortList(ComPortList.Items.Cast<string>(), ComPortList.SelectedItem as string, Port.IsOpen);
-
-            // If there was an update, then update the control showing the user the list of port names
-            if (!String.IsNullOrEmpty(selected))
-            {
-                ComPortList.ItemsSource = null;
-                ComPortList.ItemsSource = OrderedPortNames();
-                ComPortList.SelectedItem = selected;
-            }
+            Port.WriteLine("Command Received: " + strHostCommand);
         }
 
         private string[] OrderedPortNames()
@@ -54,45 +50,6 @@ namespace SerialInterpreterGUI
 
             // Order the serial port names in numberic order (if possible)
             return SerialPort.GetPortNames().OrderBy(a => a.Length > 3 && int.TryParse(a.Substring(3), out num) ? num : 0).ToArray();
-        }
-
-        private string RefreshComPortList(IEnumerable<string> PreviousPortNames, string CurrentSelection, bool PortOpen)
-        {
-            // Create a new return report to populate
-            string selected = null;
-
-            // Retrieve the list of ports currently mounted by the operating system (sorted by name)
-            string[] ports = SerialPort.GetPortNames();
-
-            // First determain if there was a change (any additions or removals)
-            bool updated = PreviousPortNames.Except(ports).Count() > 0 || ports.Except(PreviousPortNames).Count() > 0;
-
-            // If there was a change, then select an appropriate default port
-            if (updated)
-            {
-                // Use the correctly ordered set of port names
-                ports = OrderedPortNames();
-
-                // Find newest port if one or more were added
-                string newest = SerialPort.GetPortNames().Except(PreviousPortNames).OrderBy(a => a).LastOrDefault();
-
-                // If the port was already open... (see logic notes and reasoning in Notes.txt)
-                if (PortOpen)
-                {
-                    if (ports.Contains(CurrentSelection)) selected = CurrentSelection;
-                    else if (!String.IsNullOrEmpty(newest)) selected = newest;
-                    else selected = ports.LastOrDefault();
-                }
-                else
-                {
-                    if (!String.IsNullOrEmpty(newest)) selected = newest;
-                    else if (ports.Contains(CurrentSelection)) selected = CurrentSelection;
-                    else selected = ports.LastOrDefault();
-                }
-            }
-
-            // If there was a change to the port list, return the recommended default selection
-            return selected;
         }
     }
 }
